a2e4e9ced8017ba11f101a2073460403ad73ab65
atulep <aza.tulepber@gmail.com>
2017-02-18 11:40:26 -0700

    Testing the sendRequest

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ process.env.DEBUG = 'actions-on-google:*';
 let Assistant = require('actions-on-google').ApiAiAssistant;
 let express = require('express');
 let bodyParser = require('body-parser');
-let request = requre('request'); // for sending the http requests to Numbers API
+let request = require('request'); // for sending the http requests to Numbers API
 let app = express();
 app.set('port', (process.env.PORT || 8080));
 app.use(bodyParser.json({type: 'application/json'}));
@@ -21,13 +21,21 @@ const PREFIX_HAPPY = "Sure. Did you know that ";
 app.post('/', function(request, response) {
     const assistant = new Assistant({request: request, response: response});
     
+    function testSendRequest() {
+        console.log("Test...");
+        var url = "http://numbersapi.com/7/math";
+        var result = sendRequest(url);
+        console.log("Result from sendRequest is " + result);
+    } 
+    
     /**
      * An action that provides a fact based on the given number by the user. 
      */
     function provideFact(assistant) {
         var number = assistant.getArgument(NUMBER_ARGUMENT);
-        var fact = sendRequest("http://numbersapi.com/" + number); // defaults to trivia
-        var reply = PREFIX_HAPPY + fact;
+        //var fact = sendRequest("http://numbersapi.com/" + number); // defaults to trivia
+        //var reply = PREFIX_HAPPY + fact;
+        var reply = "Hello World!";
         assistant.tell(reply);
     }
     
@@ -43,8 +51,20 @@ app.post('/', function(request, response) {
         return ret_val;
     }
 
+    /**
+     * Action for the welcome. It can be equivalently defined in the API.AI console as well.
+     */
+    function welcome(assistant) {
+        var reply = "Welcome to Number Facts! What number is on your mind?";
+        assistant.tell(reply);
+    }
+    
+    testSendRequest();
     let actionMap = new Map();
+    actionMap.set(PROVIDE_FACT, provideFact);
+    actionMap.set(DEFAULT_WELCOME, welcome); 
     assistant.handleRequest(actionMap);
+    testSendRequest();
 });
 
 var server = app.listen(app.get('port'), function() {
